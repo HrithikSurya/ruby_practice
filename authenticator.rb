fb1d9b2b0113ed05d5f05844253c97b054f55f67
HrithikSurya <hrithik@webkorps.com>
2023-08-16 20:23:53 +0530

    v1.1

--- authenticator.rb
+++ authenticator.rb
@@ -5,15 +5,15 @@ def initialize(user)
 end
 end
 
-$users = [
+users = [
     { username: "hrithik", password: "hri786" },
     { username: "ashish", password: "ashi786" },
     { username: "rajeev", password: "raj786" },
     { username: "ishita", password: "ish786" },
 ]
 
-def checkuser(username ,password)
-    $users.each {    
+def checkuser(username ,password,users)
+    users.each {    
         |user|
         if (user[:username]==username && user[:password]==password)
             return User.new(user)
@@ -21,13 +21,13 @@ def checkuser(username ,password)
     }
     return false
 end
-# checkuser("jdkfk","fhjhfj")
 
 puts "This Program will take Input from User and compare password"
 puts "if correct we eill get back the user object"
 
-# attempts=1
-while true
+attempts=0
+while attempts<3
+    attempts+=1
     puts "1. Authenticator\n2.Quit"
     choice=gets.to_i
     case choice
@@ -41,16 +41,12 @@ while true
         password = gets.chomp
 
         25.times{ print "_" }
-        found=checkuser(username ,password)
-        puts found
-        
-        if (!found)
-            puts "Not Found"
-        elsif (found.class==Object)
-            "object recv."
-        else
-            puts "Credentials were Incorrect"
+        obj=checkuser(username ,password, users)
+        if (obj==false)
+            puts "User Not Found"
         end
+        puts obj
+        puts "Object Created: "
 
     when 2
         exit
